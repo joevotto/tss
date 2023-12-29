2ef02156b6b39d1128dd5c3370a009656c70077f
Joseph Votto <joevotto@users.noreply.github.com>
2023-12-28 17:36:31 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+self.addEventListener('install', async event => {
+  console.log('install event')
+});
+
+self.addEventListener('fetch', async event => 
+{
+  const req = event.request;
+  console.log('fetch event')
+  event.respondWith(cacheFirst(req));
+});
+
+async function cacheFirst(req) {
+  const cache = await caches.open(cacheName); 
+  const cachedResponse = await cache.match(req); 
+  return cachedResponse || fetch(req); 
+}
+
+const cacheName = 'Tech Support Stories';
+const staticAssets = [
+  './',
+  'index.html',
+  'favicon.ico',
+  'android-chrome-512x512.png',
+  'android-chrome-192x192.png'
+];
+
+self.addEventListener('install', async event => {
+  const cache = await caches.open(cacheName); 
+  await cache.addAll(staticAssets); 
+});
